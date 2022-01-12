43d75122fd9f69d2407835dc24651c782bea2af0
CycleDM <cyclem0508@gmail.com>
2022-01-12 21:20:26 +0900

    update scripts

--- scripts/build-helper.sh
+++ scripts/build-helper.sh
@@ -3,7 +3,7 @@ work_dir=$(dirname $(dirname $(readlink -f "$0")))
 
 docker pull steamcmd/steamcmd:alpine
 
-docker run -it --rm -v $work_dir/temp:/data steamcmd +force_install_dir /data +login anonymous +app_update 380870 +quit
+docker run -it --rm -v $work_dir/temp:/data steamcmd/steamcmd:alpine steamcmd +force_install_dir /data +login anonymous +app_update 380870 +quit
 
 cp $work_dir/dockerfile/Dockerfile $work_dir/Dockerfile
 docker build -t cycledm/project-zomboid-server $work_dir

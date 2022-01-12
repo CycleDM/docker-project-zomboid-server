c24e7385262725e922cbb90c71c233e84f664e84
CycleDM <cyclem0508@gmail.com>
2022-01-12 21:10:05 +0900

    update scripts

---
+++ scripts/build-helper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+work_dir=$(dirname $(dirname $(readlink -f "$0")))
+
+docker pull steamcmd/steamcmd:alpine
+
+docker run -it --rm -v $work_dir/temp:/data steamcmd +force_install_dir /data +login anonymous +app_update 380870 +quit
+
+cp $work_dir/dockerfile/Dockerfile $work_dir/Dockerfile
+docker build -t cycledm/project-zomboid-server $work_dir
+rm $work_dir/Dockerfile
